346f0619162804238f2f603c20db782357977bcd
ruki <waruqi@gmail.com>
2020-04-24 00:43:13 +0800

    add inject entries

--- src/lua/main.lua
+++ src/lua/main.lua
@@ -25,8 +25,47 @@ local options =
 {
     {'i', "input",     "kv", nil, "Set the input program path."}
 ,   {'o', "output",    "kv", nil, "Set the output program path."}
-,   {nil, "libraries", "v",  nil, "Set all injected dynamic libraries path list."}
+,   {nil, "libraries", "vs", nil, "Set all injected dynamic libraries path list."}
 }
+
+-- do inject for elf program
+function _inject_elf(inputfile, outputfile, libraries)
+end
+
+-- do inject for macho program
+function _inject_macho(inputfile, outputfile, libraries)
+end
+
+-- do inject for pe program
+function _inject_pe(inputfile, outputfile, libraries)
+end
+
+-- do inject for apk program
+function _inject_apk(inputfile, outputfile, libraries)
+end
+
+-- do inject for ipa program
+function _inject_ipa(inputfile, outputfile, libraries)
+end
+
+-- do inject
+function _inject(inputfile, outputfile, libraries)
+    if inputfile:endswith(".so") then
+        _inject_elf(inputfile, outputfile, libraries)
+    elseif inputfile:endswith(".dylib") then
+        _inject_macho(inputfile, outputfile, libraries)
+    elseif inputfile:endswith(".exe") then
+        _inject_pe(inputfile, outputfile, libraries)
+    elseif inputfile:endswith(".dll") then
+        _inject_pe(inputfile, outputfile, libraries)
+    elseif inputfile:endswith(".apk") then
+        _inject_apk(inputfile, outputfile, libraries)
+    elseif inputfile:endswith(".ipa") then
+        _inject_ipa(inputfile, outputfile, libraries)
+    else
+        -- TODO elf or macho program?
+    end
+end
 
 -- the main entry
 function main ()
@@ -40,4 +79,24 @@ function main ()
         opts.help()
         return
     end
+
+    -- get input file
+    local inputfile = opts.input
+    assert(os.isfile(inputfile), "%s not found!", inputfile)
+
+    -- get output file
+    local outputfile = opts.output
+    if not outputfile then
+        outputfile = path.join(path.directory(inputfile), path.basename(inputfile) .. "_injected" .. path.extension(inputfile))
+    end
+
+    -- get libraries
+    local libraries = opts.libraries
+
+    -- do inject
+    _inject(inputfile, outputfile, libraries)
+
+    -- trace
+    cprint("${bright green}inject ok!")
+    cprint("${yellow}  -> ${clear bright}%s", outputfile)
 end
