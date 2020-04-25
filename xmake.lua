3c5147c95b36121c22a0ea718fc49494415dea91
ruki <waruqi@gmail.com>
2020-04-25 23:33:11 +0800

    fix compile errors

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,15 @@ set_xmakever("2.3.2")
 add_rules("mode.debug", "mode.release")
 add_requires("libxmake", "lief")
 
+if is_plat("windows") then 
+    if is_mode("release") then
+        add_cxflags("-MT") 
+    elseif is_mode("debug") then
+        add_cxflags("-MTd") 
+    end
+    add_ldflags("-nodefaultlib:msvcrt.lib")
+end
+
 target("luject")
     add_rules("xmake.cli")
     add_files("src/lni/*.cpp")
