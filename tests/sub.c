119765c10a9d29e57eb47633fbb548a62f99f8af
ruki <waruqi@gmail.com>
2020-05-28 22:43:26 +0800

    add dll entry

--- tests/sub.c
+++ tests/sub.c
@@ -4,7 +4,14 @@ int sub(int a, int b)
 {
     return a + b;
 }
-#if !defined(_WIN32)
+#if defined(_WIN32)
+#include <windows.h>
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, PVOID lpReserved)
+{
+    printf("sub: injected ok!\n");
+    return TRUE;
+}
+#else
 void __attribute__ ((constructor)) sub_init(void)
 {
     printf("sub: injected ok!\n");
